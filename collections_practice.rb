0120d9c7708aa2647441227f1e07cbf5e1b909ad
Jaela Wesley <jcwesley93@gmail.com>
2019-02-05 17:43:43 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,34 @@
 # your code goes here
+def begins_with_r(array)
+  array.each do
+    if array.all? { |name| name.start_with?("r") } == true
+      return true
+    else return false
+    end
+  end
+end
+
+def contain_a(array)
+  array.select { |word| word.include?("a") }
+end
+
+def first_wa
+end
+
+def remove_non_strings(array)
+  array.delete_if { |word| word.class != String }
+end
+
+def count_elements(array)
+end
+
+def merge_data(keys, data)
+  keys.concat(data)
+  keys.flatten
+end
+
+def find_cool
+end
+
+def organize_schools
+end
